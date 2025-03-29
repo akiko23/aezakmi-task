baafd6a26dbfb15b4148eb1fef5495cc99c4543a
akiko23 <dmvasilenko43@gmail.com>
2025-03-29 14:15:13 +0300

    implement rate limiting

--- aezakmi_task/main.py
+++ aezakmi_task/main.py
@@ -1,10 +1,12 @@
+import asyncio
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from fastapi import FastAPI
 from dishka import AsyncContainer
-from starlette.middleware.base import BaseHTTPMiddleware
+from redis.asyncio import Redis
 
+from aezakmi_task.controllers.middlewares.rate_limiting_middleware import RateLimitMiddleware
 from aezakmi_task.controllers.notification import router as notifications_router
 from aezakmi_task.controllers.metrics import router as metrics_router
 from dishka.integrations.fastapi import setup_dishka
@@ -19,13 +21,16 @@ async def lifespan(app_: FastAPI) -> AsyncGenerator[None, None]:
 
     await app_.container.close()
 
+
 def create_app(ioc_container: AsyncContainer):
     application = FastAPI(title="Notification Service", version="1.0.0", lifespan=lifespan)
 
     setup_dishka(container=ioc_container, app=application)
-
     application.container = ioc_container
+
     application.add_middleware(RequestCountMiddleware)
+    application.add_middleware(RateLimitMiddleware, ioc_container=ioc_container)
+
     application.include_router(notifications_router, prefix="/api/v1")
     application.include_router(metrics_router)
 
@@ -38,4 +43,3 @@ def create_app(ioc_container: AsyncContainer):
 
 container = setup_di()
 app = create_app(container)
-
